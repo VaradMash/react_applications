1370f291cc65461bc44a790a3f925e63dc259c49
VaradMash <varadmash2201@gmail.com>
2021-08-02 09:37:49 +0530

    Entry point Javascript file added.

---
+++ scorekeeper_app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Header from './components/Header';
+import Holder from './components/Holder'
+
+ReactDOM.render(
+  <div>
+    <Header />
+    <Holder />
+  </div>  ,
+  document.getElementById('root')
+);
