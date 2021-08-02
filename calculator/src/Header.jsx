0c092b861f999a1bd4f454a7d9c7db98e5a7a348
VaradMash <varadmash2201@gmail.com>
2021-08-02 08:51:20 +0530

    Header component added.

---
+++ calculator/src/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css"
+
+function Header(){
+    return(
+        <div className="calculator_header">
+            <h1> Calculator</h1>
+        </div>
+    );
+}
+
+export default Header;
