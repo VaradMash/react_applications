8f91a90d95fb6bccd4b73a4f42a247fd5ad18949
VaradMash <varadmash2201@gmail.com>
2021-08-02 08:50:29 +0530

    Entry point Javascript file added.

---
+++ calculator/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+import Form from "./form"
+
+ReactDOM.render( 
+  <div>
+    <Header />
+    <Form />
+  </div>,
+document.getElementById('root'));
