afb87ce1b27ba5955d0c39d8e23d1f8024142df2
VaradMash <varadmash2201@gmail.com>
2021-08-03 12:28:58 +0530

    App component added.

---
+++ timer_app/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./Header";
+import Holder from "./Holder";
+function App(){
+    return(
+        <div>
+            <Header />
+            <Holder />
+        </div>
+    );
+}
+
+export default App;
